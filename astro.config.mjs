9e0d49a92faa554ffc09bceef1ae65b807f70906
9h8x <juannicolassack@gmail.com>
2025-03-13 19:57:24 -0300

    configured site in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://telelibre.site',
   output: "server",
   adapter: cloudflare(),
   integrations: [react()],
