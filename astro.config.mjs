47dfce00aa34ab3a8d2eaa4eb89a0ac7b003bd52
9h8x <juannicolassack@gmail.com>
2025-03-14 08:41:28 -0300

    forgot to switch back to cf runtime + removed dev things

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,30 +8,14 @@ import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://telelibre.site',
+  site: "https://telelibre.site",
   output: "server",
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: cloudflare(),
   integrations: [react()],
   vite: {
     build: {
       outDir: "dist",
       target: "esnext",
-    },
-    preview: {
-      port: 3000,
-      strictPort: true,
-      host: "0.0.0.0",
-      allowedHosts: true,
-    },
-    server: {
-      cors: {
-        origin: ["https://dev.jns.net.ar"],
-        methods: ["GET", "POST", "PUT", "DELETE"],
-        allowedHeaders: ["Content-Type"],
-      },
-      allowedHosts: ["dev.jns.net.ar"], // added this
     },
     plugins: [tailwindcss()],
     resolve: {
