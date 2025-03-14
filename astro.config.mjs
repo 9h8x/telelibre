d5c0e06e2d2ca90200a45767ac514f8621f5e961
9h8x <juannicolassack@gmail.com>
2025-03-13 21:02:09 -0300

    SEO Optimization

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,12 @@ import react from "@astrojs/react";
 
 import cloudflare from "@astrojs/cloudflare";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  site: 'https://telelibre.site',
+  integrations: [react(), sitemap()],
 
   vite: {
     build: {
